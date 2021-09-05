4bc19d4abd28234118c253b4f596a75f4b33c379
Monica Rotulo <monica.rotulo@gmail.com>
2021-09-05 15:30:18 +0200

    fixed some bugs for mobile nav toggle

--- script.js
+++ script.js
@@ -17,29 +17,42 @@ fade($('.quoteLoop > .quote').first());
 
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() > 300) {
+    if ($(window).scrollTop() > 50) {
         $('.main_nav').addClass('sticky');
+        $('.mobile-toggle').addClass('open-toggle');
     } else {
         $('.main_nav').removeClass('sticky');
+        if ($('.main_nav').hasClass('open-nav')){
+
+        }else{
+            $('.mobile-toggle').removeClass('open-toggle');
+        }
+        
     }
 });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
     if ($('.main_nav').hasClass('open-nav')) {
-        $('.main_nav').removeClass('open-nav');
+        if ($('.main_nav').hasClass('sticky')){
+            
+        } else {
+            $('.mobile-toggle').removeClass('open-toggle');
+        } 
+        $('.main_nav').removeClass('open-nav'); 
     } else {
         $('.main_nav').addClass('open-nav');
-    }
-});
-
-$('.mobile-toggle').click(function() {
-    if ($('.mobile-toggle').hasClass('open-toggle')) {
-        $('.mobile-toggle').removeClass('open-toggle');
-    } else {
         $('.mobile-toggle').addClass('open-toggle');
     }
 });
+
+// $('.mobile-toggle').click(function() {
+//     if ($('.mobile-toggle').hasClass('open-toggle')) {
+//          $('.mobile-toggle').removeClass('open-toggle');
+//     } else {
+//         $('.mobile-toggle').addClass('open-toggle');
+//     }
+// });
 
 $('.main_nav li a').click(function() {
     if ($('.main_nav').hasClass('open-nav')) {
